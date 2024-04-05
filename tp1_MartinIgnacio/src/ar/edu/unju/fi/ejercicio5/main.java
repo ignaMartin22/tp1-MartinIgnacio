fd7c5a2bd9fc76faaae114901de21aa50acfa434
fibia <fibia@GWTN156-4>
2024-04-04 22:18:44 -0300

    ejercicio 5

---
+++ tp1_MartinIgnacio/src/ar/edu/unju/fi/ejercicio5/main.java
@@ -0,0 +1,18 @@
+package ar.edu.unju.fi.ejercicio5;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		int i;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Por favor ingrese un numero entre 1 y 9: ");
+		int numero = scanner.nextInt();
+		
+		for(i = 0; i<=10; i++) {
+			System.out.println(numero+" X  "+i+" = "+numero*i);
+		}
+	}
+
+}
