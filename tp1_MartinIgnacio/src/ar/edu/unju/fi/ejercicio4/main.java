f9aea59f8e15f655312c11e2579fd2820b785741
fibia <fibia@GWTN156-4>
2024-04-04 22:06:03 -0300

    Ejercicio 4: leer aclaración

---
+++ tp1_MartinIgnacio/src/ar/edu/unju/fi/ejercicio4/main.java
@@ -0,0 +1,22 @@
+package ar.edu.unju.fi.ejercicio4;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		int i;
+		Scanner scanner = new Scanner(System.in);
+		do {  // uso el do-while solo como bucle de control para que no se permita el ingreso de un numero mayor a 10 o negativo
+			System.out.println("Ingrese numero: ");
+			i = scanner.nextInt();
+		} while(i > 10 || i < 0);
+		
+		int factorial = 1;
+		while(i > 0) {
+			factorial *= i--;
+		}
+		System.out.println("Factorial: "+factorial);
+	}
+
+}
