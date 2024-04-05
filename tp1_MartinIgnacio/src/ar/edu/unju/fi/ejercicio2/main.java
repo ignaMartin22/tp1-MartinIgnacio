0fdae4b587e3030bae8c1d6048a96614ee08683a
fibia <fibia@GWTN156-4>
2024-04-04 21:36:14 -0300

    ejercicio 2

---
+++ tp1_MartinIgnacio/src/ar/edu/unju/fi/ejercicio2/main.java
@@ -0,0 +1,21 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class main {
+
+	public static void main(String[] args) {
+		String nombrePais = "España";
+		int edad = 19;
+		float alturaEdificio= 25.3f;
+		float precio = 12.99f;
+		long numeroTelefono = 3884157277l;
+		double calculoCoseno = 0.9999619231;
+		
+		System.out.println(nombrePais);
+		System.out.println(edad);
+		System.out.println(alturaEdificio);
+		System.out.println(precio);
+		System.out.println(numeroTelefono);
+		System.out.println(calculoCoseno);
+	}
+
+}
