ec103e5eb64d183c6c174b34e36238f2cd2d4c2c
fibia <fibia@GWTN156-4>
2024-04-04 21:49:47 -0300

    ejercicio 3

---
+++ tp1_MartinIgnacio/src/ar/edu/unju/fi/ejercicio3/main.java
@@ -0,0 +1,22 @@
+package ar.edu.unju.fi.ejercicio3;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+	
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Por favor ingrese un numero: ");
+		int numero = scanner.nextInt();
+		
+		if(numero % 2 == 0) { // si el numero es par
+			System.out.println("El número ingresado es par: "+numero*3);
+		}else {
+			System.out.println("El número ingresado es impar: "+numero*2);
+		}
+		
+		scanner.close();
+	}
+
+}
