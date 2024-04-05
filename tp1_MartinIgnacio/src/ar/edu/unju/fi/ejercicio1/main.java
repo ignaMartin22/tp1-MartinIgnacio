a4f90b4601ad6bbefbdbd857c1077b6814181267
fibia <fibia@GWTN156-4>
2024-04-04 21:31:33 -0300

    Ejercicio 1

---
+++ tp1_MartinIgnacio/src/ar/edu/unju/fi/ejercicio1/main.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class main {
+
+	public static void main(String[] args) {
+		int n1 = 2;
+		int n2 = 3;
+		int n3 = 5;
+		int n4 = 6;
+		int n5 = 8;
+		
+		int promedio = (n1+n2+n3+n4+n5)/5;
+		
+		System.out.println("El promedio es: "+promedio);
+		
+		
+		
+	}
+
+}
