f5d1b26f0ad5358e170d61328f2fcff5caaa70d4
fibia <fibia@GWTN156-4>
2024-04-10 11:18:51 -0300

    ejercicio 11

---
+++ tp1_MartinIgnacio/src/ar/edu/unju/fi/ejercicio11/main/Main.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.ejercicio11.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i,j;
+		for(j=40, i=2;j>0;j--) {
+			System.out.print(i*j+" ");
+			i++;
+		}
+	}
+
+}
